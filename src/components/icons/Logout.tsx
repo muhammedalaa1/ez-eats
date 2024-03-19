bf6dfa43f57ef1e191e7acdc380e77820fdd6056
muhammedalaa1 <muhammedalaa2213@gmail.com>
2024-03-20 00:19:12 +0200

    first commit

--- src/components/icons/Logout.tsx
+++ src/components/icons/Logout.tsx
@@ -1,19 +1,18 @@
-import React from "react";
 import type { SVGProps } from "react";
 
 export function Logout(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="2rem"
-      height="2rem"
-      viewBox="0 0 24 24"
-      {...props}
-    >
-      <path
-        fill="#f13c4f"
-        d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h7v2H5v14h7v2zm11-4l-1.375-1.45l2.55-2.55H9v-2h8.175l-2.55-2.55L16 7l5 5z"
-      ></path>
-    </svg>
-  );
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="2rem"
+            height="2rem"
+            viewBox="0 0 24 24"
+            {...props}
+        >
+            <path
+                fill="#f13c4f"
+                d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h7v2H5v14h7v2zm11-4l-1.375-1.45l2.55-2.55H9v-2h8.175l-2.55-2.55L16 7l5 5z"
+            ></path>
+        </svg>
+    );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -7,11 +6,11 @@ import { AuthProvider } from "./context/Auth.tsx";
 import { ParseProvider } from "./context/parse.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <ParseProvider>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
-  </ParseProvider>
+    <ParseProvider>
+        <AuthProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </AuthProvider>
+    </ParseProvider>
 );
